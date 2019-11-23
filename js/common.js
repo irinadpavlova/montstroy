c1209715fafff3f0af077f5a81331baa80a377f5
irinadpavlova <irinadpavlova@gmail.com>
2019-11-24 01:28:07 +0300

    slider

--- js/common.js
+++ js/common.js
@@ -1,43 +1,61 @@
-var swiper = new Swiper('#swiper-partners.swiper-container', {
-  slidesPerView: 4,
-  spaceBetween: 30,
-  slidesPerGroup: 3,
-  loop: false,
-  loopFillGroupWithBlank: true,
+
+var swiper = new Swiper('.swiper-partners', {
+  loop: true,
   pagination: {
-    el: '#swiper-partners .swiper-pagination',
+    el: '.swiper-pagination',
     clickable: true,
-    dynamicBullets: true,
   },
-  navigation: {
-    nextEl: '#swiper-partners .swiper-button-next',
-    prevEl: '#swiper-partners .swiper-button-prev',
-  },
-  breakpoints: {
-    1169: {
-      slidesPerView: 3,
-      spaceBetween: 260
+  spaceBetween: 30,
+  slidesPerView: 'auto',
+  autoHeight: true,
+  breakpoints :{
+    1169:{
+      spaceBetween : 10,
+      slidesPerView: 'auto',
+      centeredSlides: true,
     },
-    723: {
-      slidesPerView: 1,
-      spaceBetween: 0
+    768:{
+      spaceBetween: 0,
+      slidesPerView: 'auto',
     }
   }
 });
 
+// var swiper = new Swiper('.swiper-advantages', {
+//   loop: true,
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+//   spaceBetween: 0,
+//   slidesPerView: 'auto',
+//   autoHeight: true,
+//   breakpoints :{
+//     1169:{
+//       spaceBetween : 10,
+//       slidesPerView: 'auto',
+//       centeredSlides: true,
+//     },
+//     768:{
+//       spaceBetween: 0,
+//       slidesPerView: 'auto',
+//     }
+//   }
+// });
+
 var menu = document.querySelector(".burger-menu");
 var navigation = document.querySelector(".main-navigation");
 var heading1 = document.querySelector(".services-item-1");
-var service1 = document.querySelector(".service-1");
 var heading2 = document.querySelector(".services-item-2");
-var service2 = document.querySelector(".service-2");
 var heading3 = document.querySelector(".services-item-3");
-var service3 = document.querySelector(".service-3");
 var heading4 = document.querySelector(".services-item-4");
-var service4 = document.querySelector(".service-4");
 var heading5 = document.querySelector(".services-item-5");
-var service5 = document.querySelector(".service-5");
 var heading6 = document.querySelector(".services-item-6");
+var service1 = document.querySelector(".service-1");
+var service2 = document.querySelector(".service-2");
+var service3 = document.querySelector(".service-3");
+var service4 = document.querySelector(".service-4");
+var service5 = document.querySelector(".service-5");
 var service6 = document.querySelector(".service-6");
 var list1 = document.querySelector(".services-list-1");
 var list2 = document.querySelector(".services-list-2");
@@ -53,30 +71,36 @@ menu.addEventListener("click", function (evt) {
 
 heading1.addEventListener("click", function (evt) {
   evt.preventDefault();
-  service1.classList.toggle("modal-show");
+  list1.classList.toggle("modal-show");
+  service1.classList.toggle("services-item-active");
 });
 
 heading2.addEventListener("click", function (evt) {
   evt.preventDefault();
-  service2.classList.toggle("modal-show");
+  list2.classList.toggle("modal-show");
+  service2.classList.toggle("services-item-active");
 });
 
 heading3.addEventListener("click", function (evt) {
   evt.preventDefault();
-  service3.classList.toggle("modal-show");
+  list3.classList.toggle("modal-show");
+  service3.classList.toggle("services-item-active");
 });
 
 heading4.addEventListener("click", function (evt) {
   evt.preventDefault();
-  service4.classList.toggle("modal-show");
+  list4.classList.toggle("modal-show");
+  service4.classList.toggle("services-item-active");
 });
 
 heading5.addEventListener("click", function (evt) {
   evt.preventDefault();
-  service5.classList.toggle("modal-show");
+  list5.classList.toggle("modal-show");
+  service5.classList.toggle("services-item-active");
 });
 
 heading6.addEventListener("click", function (evt) {
   evt.preventDefault();
-  service6.classList.toggle("modal-show");
+  list6.classList.toggle("modal-show");
+  service6.classList.toggle("services-item-active");
 });
